ac75f4a924af4671ac5d09f88406c22d3633a59f
xaoo99 <xaoo99@gmail.com>
2015-01-27 09:17:00 +0100

    Rozbudowa klasy o getery, setery i toString().

--- src/projektbiblioteka/Ksiazka.java
+++ src/projektbiblioteka/Ksiazka.java
@@ -7,10 +7,55 @@
 package projektbiblioteka;
 
 /**
- * 
- * @author student
+ * @author Mela Paulina, Migas Michal, Zimnicki Piotr
  */
 public class Ksiazka 
 {
     private String Tytul;
+    private String ISBN;
+    private Gatunek Gatunek;
+    private Autor Autor;
+    private Wydawnictwo Wydawnictwo;
+    
+    
+    public Ksiazka(String tytul, String isbn, Autor autor, Gatunek gatunek, Wydawnictwo wydawnictwo)
+    {
+        this.Tytul = tytul;
+        this.ISBN = isbn;
+        if (autor == null)
+            this.Autor = new Autor();
+        else
+            this.Autor = autor;
+        if (gatunek == null)
+            this.Gatunek = new Gatunek();
+        else
+            this.Gatunek = gatunek;
+        if (wydawnictwo == null)
+            this.Wydawnictwo = new Wydawnictwo();
+        else
+            this.Wydawnictwo = wydawnictwo;
+    }
+    public Ksiazka(){this("","",null,null,null);};
+    
+    public String GetTytul(){return this.Tytul;};
+    public String GetISBN(){return this.ISBN;};
+    public Gatunek GetGatunek(){return this.Gatunek;};
+    public Autor GetAutor(){return this.Autor;};
+    public Wydawnictwo GetWydawnictwo(){return this.Wydawnictwo;};
+    
+    public boolean SetTytul(String NowyTytul){return false;};
+    public boolean SetISBN(String NowyTytul){return false;};
+    public boolean SetGatunek(String NowyTytul){return false;};
+    public boolean SetAutor(String NowyTytul){return false;};
+    public boolean SetWydawnictwo(String NowyTytul){return false;};
+    
+    @Override
+    public String toString()
+    {
+        return this.Tytul + "\n" 
+             + this.Autor.toString() + "\n"
+             + this.Wydawnictwo.toString() + "\n"
+             + this.ISBN + "\n"
+             + this.Gatunek.toString();
+    }
 }
