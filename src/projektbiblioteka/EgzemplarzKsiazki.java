f7e4c9c56a5384716ee1a0bf42f8fbf8dab015a6
xaoo99 <xaoo99@gmail.com>
2015-01-27 13:50:00 +0100

    Rozbudowanie klasy EgzemplarzKsiazki

--- src/projektbiblioteka/EgzemplarzKsiazki.java
+++ src/projektbiblioteka/EgzemplarzKsiazki.java
@@ -7,10 +7,27 @@
 package projektbiblioteka;
 
 /**
- *
  * @author Mela Paulina, Migas Michal, Zimnicki Piotr
  */
 public class EgzemplarzKsiazki 
 {
+    private Ksiazka Tytul;
+    private int ID;
+    private static int LastId = 0;
     
+    public EgzemplarzKsiazki(Ksiazka tytul)
+    {
+        this.Tytul = tytul;
+        EgzemplarzKsiazki.LastId++;
+        this.ID = EgzemplarzKsiazki.LastId;
+    };
+    
+    public Ksiazka getTytul(){return this.Tytul;};
+    public int GetID(){return this.ID;};
+    
+    @Override
+    public String toString()
+    {
+        return "" + this.ID + "\n" + this.Tytul;
+    }
 }
